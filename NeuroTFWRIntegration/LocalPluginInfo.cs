512d10f23a242d31fd7d4d95b429876e9594340f
pandapanda135 <pandas135works@gmail.com>
2025-12-18 16:09:53 +0000

    remove github from plugin info

--- NeuroTFWRIntegration/LocalPluginInfo.cs
+++ NeuroTFWRIntegration/LocalPluginInfo.cs
@@ -2,7 +2,7 @@ namespace NeuroTFWRIntegration;
 
 public static class LocalPluginInfo
 {
-	public const string PLUGIN_GUID = "com.github.pandapanda135.NeuroTFWRIntegration";
+	public const string PLUGIN_GUID = "com.pandapanda135.NeuroTFWRIntegration";
 	public const string PLUGIN_NAME = "NeuroTFWR";
 	public const string PLUGIN_VERSION = "1.0.0";
 }
