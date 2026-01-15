7232b6b564e05a91c2cd397c9ec405b315151510
pandapanda135 <pandas135works@gmail.com>
2026-01-15 17:12:31 +0000

    Add new prefabs and change how starting components are loaded

--- NeuroTFWRIntegration/Plugin.cs
+++ NeuroTFWRIntegration/Plugin.cs
@@ -31,8 +31,8 @@ public class Plugin : BaseUnityPlugin
 
 	#region UIAssets
 
-	private static GameObject? _toastContainer;
-	public static ToastsManager? ToastsManager => _toastContainer?.GetComponent<ToastsManager>();
+	public static GameObject? ToastContainer;
+	public static ToastsManager? ToastsManager => ToastContainer?.GetComponent<ToastsManager>();
 
 	#endregion
 	
@@ -60,12 +60,10 @@ public class Plugin : BaseUnityPlugin
 		
 		Context.Send($"{Strings.StartGameContext}");
 		RegisterMainActions.PopulateActionLists();
-		AddToastsContainer();
-		CreateChat();
+		LoadComponents.LoadStartingComponents();
+		// CreateTest();
 	}
 
-	private AssetBundle? _bundle;
-	private AssetBundle? _toastBundle;
 	private int _waitNext;
 	private void Update()
 	{
@@ -85,7 +83,7 @@ public class Plugin : BaseUnityPlugin
 		{
 			var toastPath = Path.Combine(Paths.PluginPath, "NeuroTFWRIntegration", "AssetBundles", "validation-toast");
 
-			_toastBundle = AssetBundleHelper.GetAssetBundle(toastPath);
+			AssetBundleHelper.GetAssetBundle(toastPath);
 			Logger.LogInfo($"creating toast: {toastPath}");
 			var toastAsset = AssetBundleHelper.LoadBundle(toastPath,"Assets/ValidationToast.prefab");
 			if (toastAsset is null)
@@ -99,7 +97,7 @@ public class Plugin : BaseUnityPlugin
 			{
 				Utilities.Logger.Info($"i: {i}");
 				toast.Init($"text: {i}", ValidationToast.ValidationLevels.Warning);
-				_toastContainer?.GetComponent<ToastsManager>().AddToast(toastAsset);
+				ToastContainer?.GetComponent<ToastsManager>().AddToast(toastAsset);
 			}
 		}
 		
@@ -137,98 +135,4 @@ public class Plugin : BaseUnityPlugin
 		}
 	}
 
-	private static readonly string ContainerPath = Path.Combine(Paths.PluginPath, "NeuroTFWRIntegration", "AssetBundles", "toastcontainer");
-	private void AddToastsContainer()
-	{
-		if (GameObject.Find("ToastsContainer") is not null)
-		{
-			return;
-		}
-		
-		_bundle = AssetBundleHelper.GetAssetBundle(ContainerPath);
-		if (_bundle is null)
-		{
-			throw new NullReferenceException("Toast's container AssetBundle was null.");
-		}
-
-		Logger.LogInfo($"creating toast");
-		var container = AssetBundleHelper.LoadBundle(ContainerPath, "Assets/ToastsContainer.prefab");
-		if (container is null)
-		{
-			throw new NullReferenceException("container was null, there was an issue when loading it.");
-		}
-		container.AddComponent(typeof(ToastsManager));
-		var overlay = GameObject.Find("OverlayUI");
-		if (overlay is null)
-		{
-			throw new NullReferenceException($"There was an issue finding OverlayUI.");
-		}
-			
-		var containerInst = Instantiate(container, overlay.transform, false);
-			
-		containerInst.transform.localPosition = Vector3.zero;
-		containerInst.transform.localRotation = Quaternion.identity;
-		containerInst.transform.localScale = Vector3.one;
-			
-		containerInst.SetActive(true);
-		containerInst.transform.SetAsLastSibling();
-
-		Canvas canvas = containerInst.GetComponent<Canvas>();
-		if (canvas)
-		{
-			canvas.sortingOrder = 100;
-		}
-
-		_toastContainer = containerInst;
-	}
-	
-	private static readonly string ChatPath = Path.Combine(Paths.PluginPath, "NeuroTFWRIntegration", "AssetBundles", "neuro-chat");
-	private void CreateChat()
-	{
-		if (GameObject.Find("NeuroChat") is not null)
-		{
-			return;
-		}
-
-		if (!File.Exists(ChatPath))
-		{
-			Logger.LogError($"could not find neuro-chat file.");
-			return;
-		}
-		
-		AssetBundle bundle = AssetBundleHelper.GetAssetBundle(ChatPath);
-		if (bundle is null)
-		{
-			throw new NullReferenceException("Toast's container AssetBundle was null.");
-		}
-
-		Logger.LogInfo($"creating chat");
-		var chat = AssetBundleHelper.LoadBundle(ChatPath, "Assets/NeuroChat.prefab");
-		if (chat is null)
-		{
-			throw new NullReferenceException("container was null, there was an issue when loading it.");
-		}
-		chat.AddComponent(typeof(NeuroChat));
-		var overlay = GameObject.Find("OverlayUI");
-		if (overlay is null)
-		{
-			throw new NullReferenceException($"There was an issue finding OverlayUI.");
-		}
-		
-		Logger.LogInfo($"instantiating NeuroChat");
-		var chatInst = Instantiate(chat, overlay.transform, false);
-		
-		// var rect = chatInst.GetComponent<RectTransform>();
-		// rect.anchorMin = Vector2.zero;
-		// rect.anchorMax = Vector2.one;
-		// rect.offsetMin = Vector2.zero;
-		// rect.offsetMax = Vector2.zero;
-		
-		// chatInst.transform.localPosition = new Vector3(0,0,0);
-		// chatInst.transform.position = new Vector3(0,0,0);
-		// chatInst.transform.localRotation = Quaternion.identity;
-		// chatInst.transform.localScale = Vector3.one;
-		
-		chatInst.SetActive(true);
-	}
 }
